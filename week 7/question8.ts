e78c62de9954ca269f105936331e7305f9bc4137
hassansaeed1 <hassan.saeed.piaic@gmail.com>
2023-12-20 16:28:33 +0500

    working

--- week 7/question8.ts
+++ week 7/question8.ts
@@ -1,3 +1,18 @@
 /*Implement a program that takes a list of temperatures in Celsius as input from the user. Convert each
  temperature to Fahrenheit using the formula F = (C * 9/5) + 32 and store the converted temperatures in
   an array. Use a for loop to perform the conversion for each temperature.*/
+
+
+/*
+  function CelsiusToFahrenheit(Celsius:number){
+    return (Celsius*9/5)+35
+}
+function FahrenheitToCelsius(Fahrenheit:number){
+    return (Fahrenheit-35)*5/9
+}
+var Celsius:number=100
+console.log(`${Celsius}C is equal to ${CelsiusToFahrenheit(Celsius)}F`)
+var Fahrenheit:number=80
+console.log(`${Fahrenheit}F is equal to ${FahrenheitToCelsius(Fahrenheit)}C`)
+
+*/

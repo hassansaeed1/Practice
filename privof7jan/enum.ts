932909f83620a109da5554d19dd7be12866a0a28
hassansaeed1 <hassan.saeed.piaic@gmail.com>
2024-01-10 05:00:44 +0500

    doing

---
+++ privof7jan/enum.ts
@@ -0,0 +1,8 @@
+enum directions{
+    north,
+    easth,
+    west,
+    south,
+}
+let Directions=directions.north
+console.log(Directions)
---
+++ privof7jan/enum2.ts
@@ -0,0 +1,10 @@
+enum directions{
+    north = 2,
+    easth,
+    west,
+    south,
+}
+let Directions=directions.north
+console.log(Directions)
+
+console.log(directions.west)
